733c8db21e70f81ccf87752c935130cb39ba3bfd
April Mulvey <april.mulvey@gmail.com>
2016-10-23 02:10:29 +0000

    changed .env file

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 $(function () {
   //
   function renderTweet(tweet) {
--- server/lib/db.js
+++ server/lib/db.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 "use strict";
 
 const MongoClient = require("mongodb").MongoClient;
